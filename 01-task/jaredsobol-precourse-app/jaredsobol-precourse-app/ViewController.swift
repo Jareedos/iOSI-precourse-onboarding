9e2b88d05d3f0f10ddc5f8625f4bccc1c00c3213
Jareedos <jaredallens@gmail.com>
2017-09-07 15:13:16 -0700

    finished prework app

---
+++ 01-task/jaredsobol-precourse-app/jaredsobol-precourse-app/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  jaredsobol-precourse-app
+//
+//  Created by Jared Sobol on 9/7/17.
+//  Copyright © 2017 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myImage: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
